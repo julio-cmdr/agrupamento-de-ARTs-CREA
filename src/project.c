2d0219df3a9dc4c4c1cf0d25f145316c0e08ac7b
julio.cmdr <julio.cmdr@gmail.com>
2020-03-30 23:23:43 -0300

    Agrupando as combinações

--- src/project.c
+++ src/project.c
@@ -2,6 +2,7 @@
 
 Project* read_file(char *file, int *n, float *sum){
     FILE *arq;
+    *sum = 0;
 	arq = fopen(file, "r");
     Project *projects;
 
@@ -14,6 +15,7 @@ Project* read_file(char *file, int *n, float *sum){
         projects = (Project*)malloc(*n * sizeof(Project));
         for (int i = 0; i < *n; i++){
             fscanf(arq, "%d %f", &projects[i].id, &projects[i].art);
+            *sum += projects[i].art;
         }
     }
     
